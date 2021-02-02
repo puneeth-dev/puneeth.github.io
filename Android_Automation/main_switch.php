7a6b44592928b1d5b3a2046936f3d2b78eb7aa38
puneeth-dev <69901463+puneeth-dev@users.noreply.github.com>
2021-02-03 01:53:16 +0530

    Add files via upload

--- Android_Automation/main_switch.php
+++ Android_Automation/main_switch.php
@@ -1,8 +1,9 @@
 <?php 
 //echo "STOP";
+//error_reporting(0);
 include_once("C:\\xampp\\htdocs\\Embedded_SQA\\PHP/class.execute_query.php");
 $exe_query=new ExecuteQuery();
-$android_id=$_GET["device_id"];
+$android_id=$_GET["android_id"];
 $device_ip=$_GET["ip"];
 $battery_percent=$_GET["battery_percent"];
 $battery_state=$_GET["battery_state"];
@@ -20,6 +21,8 @@ if($result==0){
     $action="register";
 }else if($result[0]['command']==10){
     $action="android-command-START";
+}else if($result[0]['command']==20){
+    $action="android-command-InProgress";
 }else if($result[0]['command']==30){
     $action="android-command-STOP";
 }else{
@@ -35,6 +38,29 @@ if($result==0){
                     VALUES ( '".$android_id."', '0', '".$device_ip."', $battery_percent,$battery_state)";
             $result=$exe_query->getObject($query);
             echo "REGISTERED";
+        break;
+        case "android-command-InProgress":
+
+            $file_name = $android_id.".txt"; //name of your file
+            $server_path = "uploads/"; //server path to folder
+            $web_path = "http://192.168.0.102/Embedded_SQA/Android_Automation/uploads/"; //web path to folder
+
+            //$file_name = date("U").".txt"; //name of your file
+            
+            $file = $server_path.$file_name;
+            file_put_contents($file,"");
+            
+            $fp = fopen("php://input", 'r');
+            while ($buffer =  fread($fp, 8192)) {
+                file_put_contents($file,$buffer,FILE_APPEND | LOCK_EX);
+            }
+
+            //echo $web_path.$file_name;
+            $query="UPDATE automation.android_automation SET `command` = '20' , `log_file_path`= '".$web_path.$file_name."', `battery_percent` = '$battery_percent',
+            `battery_state` = '$battery_state',`last_update`=CURRENT_TIMESTAMP WHERE `android_id` = '$android_id'";
+            $result=$exe_query->getObject($query);
+
+            echo "InProgress";
         break;
         case "android-command-START":
             $query="UPDATE automation.android_automation SET `command` = '20' , `battery_percent` = '$battery_percent',
